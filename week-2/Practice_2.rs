c0f46cf90589f4792aca69b9f7742a002b0a20aa
Sochikayima <petermadiebo@Kays-book.local>
2024-10-23 18:35:50 +0100

    (my second commit)

---
+++ week-2/Practice_2.rs
@@ -0,0 +1,11 @@
+ fn main() {
+
+   println!(); // prints just a
+
+   println!("hello");//prints 
+
+   println!("format {} arguments", "some"); //print for some arguments 
+
+   println!("My name is {}. I am the class rep of computer science stream 1", "Sochikayima Madiebo");
+
+ }
