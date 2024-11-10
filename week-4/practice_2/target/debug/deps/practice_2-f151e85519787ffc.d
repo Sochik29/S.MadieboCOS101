e76ea43aa8015bbfea4e87d608a597daa18ef86f
Sochikayima <petermadiebo@Kays-book.local>
2024-11-11 00:00:50 +0100

    (more commits)

---
+++ week-4/practice_2/target/debug/deps/practice_2-f151e85519787ffc.d
@@ -0,0 +1,5 @@
+/Users/petermadiebo/S.MadieboCOS101/week-4/practice_2/target/debug/deps/practice_2-f151e85519787ffc: src/main.rs
+
+/Users/petermadiebo/S.MadieboCOS101/week-4/practice_2/target/debug/deps/practice_2-f151e85519787ffc.d: src/main.rs
+
+src/main.rs:
