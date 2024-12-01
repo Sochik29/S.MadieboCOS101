ad3fb0da18063a54a80682bca4d177b21b9588b6
Sochikayima <petermadiebo@Kays-book.local>
2024-12-02 00:15:19 +0100

    (new commits)

---
+++ week-7/practice_8/target/debug/deps/practice_8-627060c2075e1862.d
@@ -0,0 +1,5 @@
+/Users/petermadiebo/S.MadieboCOS101/week-7/practice_8/target/debug/deps/practice_8-627060c2075e1862: src/main.rs
+
+/Users/petermadiebo/S.MadieboCOS101/week-7/practice_8/target/debug/deps/practice_8-627060c2075e1862.d: src/main.rs
+
+src/main.rs:
---
+++ week-7/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+/Users/petermadiebo/S.MadieboCOS101/week-7/practice_9/target/debug/practice_9: /Users/petermadiebo/S.MadieboCOS101/week-7/practice_9/src/main.rs
